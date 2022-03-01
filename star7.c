2f3ea4a550212396a77979f1f3201eea572421a5
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-03-01 18:22:10 +0530

    pattern_7

---
+++ star7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("enter the number of elements:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++,printf("\n"))
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("*");
+		}
+	}
+}
